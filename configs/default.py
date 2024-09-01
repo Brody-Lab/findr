e7f46332eb68ca34509fe13470953adf2115990b
Tim Kim <tdkim@princeton.edu>
2024-08-31 21:41:50 -0400

    Update default.py

--- configs/default.py
+++ configs/default.py
@@ -8,24 +8,21 @@ def get_config(config_id):
   k_cv_set = [1, 2, 3, 4, 5] # 5-fold cross-validation
   learning_rates = 10**np.linspace(-2, -0.5, num=5)
   features_set = [30, 50, 100]
-  beta_set = [2]
   net_size = [50, 100, 200]
 
   hps = []
   for i in range(len(k_cv_set)):
     for j in range(len(learning_rates)):
       for k in range(len(features_set)):
-        for l in range(len(beta_set)):
-          for m in range(len(net_size)):
-              hps.append(
-                (
-                  k_cv_set[i], 
-                  learning_rates[j], 
-                  features_set[k],
-                  beta_set[l],
-                  net_size[m]
-                )
-              )
+        for l in range(len(net_size)):
+          hps.append(
+            (
+              k_cv_set[i], 
+              learning_rates[j], 
+              features_set[k],
+              net_size[l]
+            )
+          )
 
   """Get the default hyperparameter configuration."""
   config = ml_collections.ConfigDict()
@@ -34,8 +31,8 @@ def get_config(config_id):
   config.base_learning_rate = hps[int(config_id)-1][1]
   config.features_prior = [hps[int(config_id)-1][2]]
   config.features_posterior = [hps[int(config_id)-1][2]]
-  config.beta = hps[int(config_id)-1][3]
-  config.inference_network_size = hps[int(config_id)-1][4]
+  config.inference_network_size = hps[int(config_id)-1][3]
+  config.beta = 2.
   config.noise_level = 1.
   config.cosine_epochs = 190
   config.alpha = 0.1 # \Delta t / \tau : \Delta t = 10ms = 0.01s
